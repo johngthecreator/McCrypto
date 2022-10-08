1d4d21e9ba5dfd246baa8512d77bff5cbb26ed90
John Gorriceta <79299314+jongee0w0@users.noreply.github.com>
2022-10-07 20:08:16 -0600

    updated meta tags and svg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ function Home(props) {
                     <img src='./cryptoBuffett.png' className='md:w-1/3'/>
                     <p className='text-white text-xl font-bold text-center md:text-left'>Warren Buffet is known as the 5th richest man in the World. The success of his investing firm, Berkshire Hathaway, is attributed to his almost superhuman ability to beat the market. Legends claim that his superpower comes from his odd choice of a morning breakfast. When the market is up, Buffett is known to eat a Bacon Egg and Cheese Biscuit from fast-food franchise McDonalds. Though when the market is down, Buffett opts for the cheaper Sausage McMuffin from McDonalds.</p>
                 </div>
-                <div className='grid p-4'>
+                <div className='grid p-4 mb-[75px]'>
                     <h1 className='text-white my-[40px] text-center font-extrabold text-[40px] md:text-[50px]'>What does McCrypto do?</h1>
                     <p className='text-white text-xl font-bold text-center'>McCrypto makes it simple to keep track of rapidly changing crypto prices. McCrypto compares the prices of a crypto asset from the day before to its current price now. If the price of the asset increased from the day before, McCrypto will recommend a Bacon Egg and Cheese Biscuit. If the price of the asset decreased from the day before, McCrypto will recommend a Sausage McMuffin.</p>
                 </div>
